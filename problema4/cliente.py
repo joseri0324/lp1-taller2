96b5f0e2509edac75ec14c532174574102a6ea14
Diego Fernando Marin <dfmarin@gmail.com>
2025-09-15 15:04:49 -0500

    feat: añadir archivos problema4

---
+++ problema4/cliente.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Problema 4: Servidor HTTP básico - Cliente
+Objetivo: Crear un cliente HTTP que realice una petición GET a un servidor web local
+"""
+
+import http.client
+
+# TODO: Definir la dirección y puerto del servidor HTTP
+
+# TODO: Crear una conexión HTTP con el servidor
+# HTTPConnection permite establecer conexiones HTTP con servidores
+
+# TODO: Realizar una petición GET al path raíz ('/')
+# request() envía la petición HTTP al servidor
+# Primer parámetro: método HTTP (GET, POST, etc.)
+# Segundo parámetro: path del recurso solicitado
+
+# TODO: Obtener la respuesta del servidor
+# getresponse() devuelve un objeto HTTPResponse con los datos de la respuesta
+
+# TODO: Leer el contenido de la respuesta
+# read() devuelve el cuerpo de la respuesta en bytes
+
+# TODO: Decodificar los datos de bytes a string e imprimirlos
+# decode() convierte los bytes a string usando UTF-8 por defecto
+
+# TODO: Cerrar la conexión con el servidor
+
---
+++ problema4/servidor.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Problema 4: Servidor HTTP básico - Servidor
+Objetivo: Implementar un servidor web simple que responda peticiones HTTP GET
+y sirva archivos estáticos comprendiendo headers HTTP
+"""
+
+import http.server
+import socket
+
+# TODO: Definir la dirección y puerto del servidor HTTP
+
+class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
+    """
+    Manejador personalizado de peticiones HTTP.
+    Hereda de SimpleHTTPRequestHandler que proporciona funcionalidad básica
+    para servir archivos estáticos y manejar peticiones HTTP.
+    
+    SimpleHTTPRequestHandler incluye:
+    - Servicio de archivos estáticos desde el directorio actual
+    - Manejo de métodos HTTP GET y HEAD
+    - Generación automática de listados de directorios
+    - Headers HTTP básicos (Content-Type, Content-Length, etc.)
+    """
+    pass
+    # Nota: Al no sobreescribir ningún método, se usa el comportamiento por defecto
+    # que sirve archivos del directorio actual y genera listados de directorios
+
+# TODO: Crear una instancia de servidor HTTP
+# HTTPServer maneja las conexiones entrantes y delega el procesamiento
+# de peticiones al manejador especificado (MyRequestHandler)
+# Parámetros:
+# - (HOST, PORT): Dirección y puerto donde escuchar
+# - MyRequestHandler: Clase que manejará las peticiones HTTP
+
+# TODO: Iniciar el servidor y ponerlo en ejecución continua
+# serve_forever() maneja peticiones indefinidamente hasta una interrupción
+# (normalmente con Ctrl+C en la terminal)
+
